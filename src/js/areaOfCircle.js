0d5afdab8ff4c04a586d2b9d073d817415c6d681
Sanjay Rai <sanjaywryee@gmail.com>
2023-05-16 16:23:53 +0545

    First commit

---
+++ src/js/areaOfCircle.js
@@ -0,0 +1,11 @@
+const area = () => {
+    let r = Number(document.getElementById("circle").value) ;
+    let a = Math.PI * r *r;
+    document.getElementById("show").innerHTML= a;
+};
+
+
+
+function myFunction() {
+    alert("Hello Hacker ");
+  }
